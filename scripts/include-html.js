41e4f77f6583a3fd2d4673931e1149c431ccd289
Infin8gamer1 <Jacob@mjhsoftware.com>
2019-10-31 14:29:55 -0600

    Auctual Content

--- scripts/include-html.js
+++ scripts/include-html.js
@@ -1,15 +1,17 @@
-var header =  '<div class="header">' +
+var header =  '<header>' +
                     '<h1>PI <span class="colorRed">Services</span></h1>' +
                     //  '<img id="logo" src="images/pi-services-logo.png"/>'+
-                 '</div>' + 
+                 '</header>' + 
 
-                '<header id="navbar">' +
-                    '<div id="navbar-inner">' +
-                        '<a href="index.html"><img class="logo" src="images/pi-services-logo-emblem-only.png"/></a>' +
-                        '<a href="news.html">News</a>' +
-                        '<a href="contact.html">Contact</a>' +
-                    '</div>' +
-                '</header>';
+                '<div id="navbar">' +
+                    '<ul id="navbar-inner">' +
+                        '<li><a href="index.html"><img class="logo" src="images/pi-services-logo-emblem-only.png"/></a></li>' +
+                        '<li><a href="jacob.html">Jacob</a></li>' +
+                        '<li><a href="jameson.html">Jameson</a></li>' +
+                        '<li><a href="news.html">News</a></li>' +
+                        '<li><a href="contact.html">Contact</a></li>' +
+                    '</ul>' +
+                '</div>';
 
 function includeHeader() {
 
@@ -26,11 +28,11 @@ function includeHeader() {
     for (let i = 0; i < navbar.children.length; i++) {
         var element = navbar.children[i];
         
-        var page = element.getAttribute("href");
+        var page = element.children[0].getAttribute("href");
 
         if(page){
             if(page == file){
-                element.setAttribute("class", "active");
+                element.children[0].setAttribute("class", "active");
                 break;
             }
         }
